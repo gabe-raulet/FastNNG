5a798f3102d39be81d3a6c6b03b2a65ed538c629
Gabriel Raulet <gabe.h.raulet@gmail.com>
2026-02-02 13:54:30 -0800

    implement basic utilities

---
+++ include/utils.h
@@ -0,0 +1,60 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#undef NDEBUG
+#include <assert.h>
+
+#include <stdexcept>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <random>
+#include <deque>
+#include <tuple>
+#include <type_traits>
+#include <algorithm>
+#include <mpi.h>
+
+using Index = int64_t;
+using Real = float;
+
+#ifdef MPI_INDEX
+#undef MPI_INDEX
+#endif
+
+#define MPI_INDEX MPI_INT64_T
+
+#ifdef MPI_REAL
+#undef MPI_REAL
+#endif
+
+#define MPI_REAL MPI_FLOAT
+
+
+using IndexVector = std::vector<Index>;
+using RealVector = std::vector<Real>;
+
+template <class Iter>
+std::string container_repr(Iter first, Iter last);
+
+#define CONTAINER_REPR(container) (container_repr((container).begin(), (container).end()).c_str())
+
+void selection_sample(Index range, Index size, IndexVector& sample, int seed);
+std::string format_large_number(Index number, int prec);
+
+#define LARGE(number) format_large_number((number), (1)).c_str()
+
+int get_comm_rank(MPI_Comm comm);
+int get_comm_size(MPI_Comm comm);
+
+template <class T> MPI_Datatype mpi_type();
+
+#include "utils.hpp"
+
+#endif
